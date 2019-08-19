c40ef94915563b27a08372dbf159359242e11fd5
Casper444 <39726400+Casper444@users.noreply.github.com>
2019-08-19 10:44:09 +0200

    ny

--- digt.py
+++ digt.py
@@ -2,5 +2,7 @@ print ("""Nerver og tilstande, personer
 og fingres ord af mand
 og frygtelige vibrationer ved
 stederne, dette er mishandlinger,
+Rolf var uren, og oplevede mange blodsprængninger
+både i og ude for turen
 frakkerne sker og sker i
 fiktioner af de hviskende""")
