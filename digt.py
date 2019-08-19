32a1e1f36a387c81f53ea4604c010b8511285a1c
Casper444 <39726400+Casper444@users.noreply.github.com>
2019-08-19 10:41:09 +0200

    hi

---
+++ digt.py
@@ -0,0 +1,6 @@
+print ("""Nerver og tilstande, personer
+og fingres ord af mand
+og frygtelige vibrationer ved
+stederne, dette er mishandlinger,
+frakkerne sker og sker i
+fiktioner af de hviskende""")
