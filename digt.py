fa59c4753aa649f7d61ea80a8b0c133bcb12eb23
Mikkel Andreasen <andreasenmikkel1@gmail.com>
2019-08-19 10:46:21 +0200

    HEj jesg skrev pik

--- digt.py
+++ digt.py
@@ -1,7 +1,6 @@
-print ("""Nerver og tilstande, personer
-og fingres ord af mand
+print ("""Nerver and tilstande, personer
+og fingres ord af pik
 og frygtelige vibrationer ved
 stederne, dette er mishandlinger,
-Rolf var uren, og oplevede mange blodsprængninger
-både i og ude for turen
-fiktioner af de hviskende""")
+frakkerne sker og sker i
+underlivet af de hviskende""")
