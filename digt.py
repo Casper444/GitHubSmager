ae6e650eec52aa6b36d543cb6bda256cf74132a8
Casper444 <39726400+Casper444@users.noreply.github.com>
2019-08-19 10:44:46 +0200

    ny igen

--- digt.py
+++ digt.py
@@ -4,5 +4,4 @@ og frygtelige vibrationer ved
 stederne, dette er mishandlinger,
 Rolf var uren, og oplevede mange blodsprængninger
 både i og ude for turen
-frakkerne sker og sker i
 fiktioner af de hviskende""")
