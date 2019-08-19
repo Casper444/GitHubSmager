20ee6a28ca219cd08b670433a06afdb39f2a8c2a
Mikkel Andreasen <andreasenmikkel1@gmail.com>
2019-08-19 10:50:25 +0200

    1.0

--- digt.py
+++ digt.py
@@ -2,7 +2,7 @@
 print ("""Nerver and tilstande, personer
 og fingres ord af pik
 og frygtelige vibrationer ved
-stederne, dette er mishandlinger,
-Rolf var uren, og oplevede mange blodsprængninger
-både i og ude for turen
+underlivet, dette er mishandlinger,
+Rolf var uren spade, og oplevede mange blodsprængninger
+både i og ude for naturen
 fiktioner af de hviskende""")
